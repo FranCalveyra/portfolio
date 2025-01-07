25bcb969850c07b530c00cba20d9a48a0d49441d
FranCalveyra <franciscocalveyra24@gmail.com>
2025-01-07 10:42:36 -0300

    Changed Navbar

--- src/stories/Navbar/NavbarItem/index.tsx
+++ src/stories/Navbar/NavbarItem/index.tsx
@@ -17,7 +17,7 @@ export const NavbarItem = ({ text, href }: NavbarItemProps) => {
     };
 
     return (
-        <div className="h-16 text-center text-white text-2xl font-normal font-['Martian_Mono'] px-4 mt-6">
+        <div className="h-16 text-center text-white text-2xl font-normal font-['Martian_Mono'] px-4 mt-6 hover:underline">
             <a href={href} onClick={handleClick}>
                 {text}
             </a>
--- src/stories/Navbar/index.tsx
+++ src/stories/Navbar/index.tsx
@@ -3,7 +3,7 @@ import {NavbarItem} from "./NavbarItem/index.tsx";
 
 const Navbar = () => {
     return (
-        <div className="w-full h-[106px] px-8 py-[21px] bg-[#252a44] shadow-[0px_10px_4px_0px_rgba(0,0,0,0.25)] flex justify-center items-center">
+        <div className="w-full fixed top-0 z-50 h-[106px] px-8 py-[21px] bg-[#252a44] shadow-[0px_10px_4px_0px_rgba(0,0,0,0.25)] flex justify-center items-center">
             <div className="flex justify-center items-center gap-12">
                 <NavbarItem text={"Home"} href={"#home"}></NavbarItem>
                 <NavbarItem text={"About Me"} href={"#aboutme"}></NavbarItem>
